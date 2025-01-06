2a2284cab4dccd67d4fb8dede75d7d8a9dd48196
PapouMarc <gamermarc090@gmail.com>
2025-01-06 11:24:35 +0100

    ajout multiligne

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
-result=`python3 ${1}`
-
-echo ::set-output name=result-python3::${result}
+result=`python3 $@`
+nb_line=`echo -e "${result}" | wc -l`
+if [ ${nb_line} -gt 1 ]; then
+  echo "resultpython3<<EOF" >> $GITHUB_OUTPUT
+  echo "${result}" >> $GITHUB_OUTPUT
+  echo "EOF" >> $GITHUB_OUTPUT  
+else
+  echo "resultpython3=${result}" >> $GITHUB_OUTPUT
+fi
